0beef1e12cbfbc1d71531d39cd2becca8a3a99d8
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-10-08 14:32:41 +0500

    style inheritance

--- App.js
+++ App.js
@@ -16,28 +16,13 @@ import {
 import Greet from "./components/Greet";
 const uniImage = require("./assets/uni.jpg");
 
+// there is no css inheritance in react native as in web, but inheritance from same type of components is possible as from text to text child component
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={[
-          styles.box,
-          styles.lightBlueBg,
-          styles.boxShadow,
-          styles.androidElevation,
-        ]}
-      >
-        <Text>Light Blue Box</Text>
-      </View>
-      <View
-        style={[
-          styles.box,
-          styles.lightGreenBg,
-          styles.boxShadow,
-          styles.androidElevation,
-        ]}
-      >
-        <Text>Light Green Box</Text>
+      <View style={styles.darkBg}>
+        <Text style={styles.textWhite}>Hello, world!</Text>
       </View>
       <StatusBar backgroundColor="red" />
     </View>
@@ -49,6 +34,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  darkBg: {
+    backgroundColor: "black",
+  },
+  textWhite: {
+    color: "white",
   },
   box: {
     width: 250,
@@ -76,3 +67,26 @@ const styles = StyleSheet.create({
     elevation: 20, // work on android only
   },
 });
+
+{
+  /* <View
+        style={[
+          styles.box,
+          styles.lightBlueBg,
+          styles.boxShadow,
+          styles.androidElevation,
+        ]}
+      >
+        <Text>Light Blue Box</Text>
+      </View>
+      <View
+        style={[
+          styles.box,
+          styles.lightGreenBg,
+          styles.boxShadow,
+          styles.androidElevation,
+        ]}
+      >
+        <Text>Light Green Box</Text>
+      </View> */
+}
