4325616687673f19f4b22841d1ca7a6b5f30bcba
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-10-08 12:27:07 +0500

    addes ActivityIndicator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
+  ActivityIndicator,
   Button,
   Image,
   ImageBackground,
@@ -14,14 +15,14 @@ import {
 const uniImage = require("./assets/uni.jpg");
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-      </Text>
+      <ActivityIndicator />
+      <ActivityIndicator size={"small"} />
+      <ActivityIndicator size={"large"} color={"midnightblue"} />
+      <ActivityIndicator animating={isLoading} />
+      <Button title="Load Data" onPress={() => setIsLoading(true)} />
       <StatusBar backgroundColor="red" />
     </View>
   );
@@ -33,5 +34,6 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // justifyContent: "center",
     // padding: 20,
+    marginTop: 50,
   },
 });
