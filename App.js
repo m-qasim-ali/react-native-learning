0239a32904fff93e4a638bdd33ee409acb28092a
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-10-08 11:09:46 +0500

    ScrollView

--- App.js
+++ App.js
@@ -1,17 +1,85 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 const uniImage = require("./assets/uni.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world.</Text>
-      <Image style={{ width: 200, height: 200 }} source={uniImage} />
-      <ImageBackground source={require("./assets/uni.jpg")}>
+      <ScrollView>
         <Text>
-          Insidefff
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue,
+          egetlorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue,
+          egetlorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
         </Text>
-      </ImageBackground>
+        <Image style={{ width: 200, height: 200 }} source={uniImage} />
+        <ImageBackground
+          style={{ width: 200, height: 200 }}
+          source={require("./assets/uni.jpg")}
+        >
+          <Text>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
+            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
+          </Text>
+        </ImageBackground>
+      </ScrollView>
     </View>
   );
 }
@@ -22,5 +90,6 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
   },
 });
