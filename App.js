a7ac42c906a2a05a7cb6c093158305dd9230da4b
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-10-08 17:50:26 +0500

    DimensionAPI

--- App.js
+++ App.js
@@ -1,28 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  ActivityIndicator,
-  Alert,
-  Button,
-  Image,
-  ImageBackground,
-  Modal,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { useEffect, useState } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import Greet from "./components/Greet";
 const uniImage = require("./assets/uni.jpg");
 
 // there is no css inheritance in react native as in web, but inheritance from same type of components is possible as from text to text child component
 
 export default function App() {
+  const [window, setWindow] = useState(Dimensions.get("window"));
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", () => {
+      setWindow(Dimensions.get("window"));
+    });
+
+    return () => subscription.remove();
+  });
+
+  const width = window.width;
+  const height = window.height;
+  console.log("width: ", width, "height: ", height);
+
   return (
     <View style={styles.container}>
-      <View style={styles.darkBg}>
-        <Text style={styles.textWhite}>Hello, world!</Text>
+      <View
+        style={[
+          styles.box,
+          styles.lightBlueBg,
+          styles.boxShadow,
+          styles.androidElevation,
+          {
+            width: width > 360 ? "70%" : "90%",
+            height: width > 360 ? 250 : 300,
+          },
+        ]}
+      >
+        <Text style={{ fontSize: width > 360 ? 30 : 24 }}>Light Blue Box</Text>
       </View>
       <StatusBar backgroundColor="red" />
     </View>
@@ -42,14 +55,14 @@ const styles = StyleSheet.create({
     color: "white",
   },
   box: {
-    width: 250,
-    height: 250,
     padding: 20,
     borderRadius: 10,
     marginVertical: 10,
     borderWidth: 2,
     borderColor: "purple",
     borderStyle: "dotted",
+    justifyContent: "center",
+    alignItems: "center",
   },
   lightBlueBg: {
     backgroundColor: "lightblue",
