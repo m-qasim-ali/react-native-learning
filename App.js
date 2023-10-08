459539f44e46d4d972ce74fa4c46a2358cdff669
m-qasim-ali <muhammadqasimali96@gmail.com>
2023-10-08 11:18:14 +0500

    core component--button

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import {
+  Button,
   Image,
   ImageBackground,
   ScrollView,
@@ -12,74 +13,11 @@ const uniImage = require("./assets/uni.jpg");
 export default function App() {
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Text>
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue,
-          egetlorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue,
-          egetlorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-        </Text>
-        <Image style={{ width: 200, height: 200 }} source={uniImage} />
-        <ImageBackground
-          style={{ width: 200, height: 200 }}
-          source={require("./assets/uni.jpg")}
-        >
-          <Text>
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-            consequat nisl tortor quis velit. Donec euismod, neque nec ultricies
-            lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            tincidunt, quam eu aliquet tincidunt, diam elit commodo augue, eget
-          </Text>
-        </ImageBackground>
-      </ScrollView>
+      <Button
+        title="click"
+        color="red"
+        onPress={() => console.log("Button pressed")}
+      />
     </View>
   );
 }
@@ -87,7 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
